6098804abe6553185e8000eedfe81eaac525340e
Jeremy Kim <thk116@gmail.com>
2019-05-06 14:19:45 -0700

    edited title value in ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-deploy';
+  title = 'd e m o - d e p l o y (this has been brought to you by interpolation)';
 }
